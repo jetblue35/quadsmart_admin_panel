fe2d1077da550771b32a0ea8d548e1b6c52890dc
mamamia140o <m-h-k140@hotmail.com>
2024-01-23 23:14:38 +0300

    23:14

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ const LoginPage = () => {
       api_key: process.env.API_KEY
     }
 
-    const response = await fetch(`http://rental_management_api.railway.internal:443/v1/auth/login/server/admin`, {
+    const response = await fetch(`https://rentalmanagementapi-production.up.railway.app/v1/auth/login/server/admin`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
